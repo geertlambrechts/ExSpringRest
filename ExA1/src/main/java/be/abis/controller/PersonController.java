d797b3e724e98b0c06508f36bd3f8f1631a61461
geertlambrechts <geert.lambrechts@bnpparibasfortis.com>
2020-10-14 14:46:36 +0200

    ExA1 can't run

---
+++ ExA1/src/main/java/be/abis/controller/PersonController.java
@@ -0,0 +1,23 @@
+package be.abis.controller;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RestController;
+
+import be.abis.exercise.model.Person;
+import be.abis.exercise.service.PersonService;
+
+@RestController
+public class PersonController {
+	@Autowired
+	PersonService personService;
+	
+	@GetMapping("persons/{id}")
+	public Person findPerson(@PathVariable("id") int personId) {
+		Person p = personService.findPerson(personId);
+		return p;
+	}
+	
+
+}

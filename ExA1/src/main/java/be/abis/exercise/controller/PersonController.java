7bfa6567a1155e0f1f603713047e883f07652d0d
geertlambrechts <geert.lambrechts@bnpparibasfortis.com>
2020-10-14 15:45:09 +0200

    ExA2 add person with problem

--- ExA1/src/main/java/be/abis/exercise/controller/PersonController.java
+++ ExA1/src/main/java/be/abis/exercise/controller/PersonController.java
@@ -1,21 +1,26 @@
 package be.abis.exercise.controller;
 
+import java.io.IOException;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import be.abis.exercise.model.Person;
 import be.abis.exercise.service.PersonService;
 
 @RestController
+@RequestMapping("persons")
 public class PersonController {
 	@Autowired
 	PersonService personService;
 	
-	@GetMapping("persons/{id}")
+	@GetMapping("{id}")
 	public Person findPerson(@PathVariable("id") int personId) {
 		Person p = personService.findPerson(personId);
 		return p;
@@ -25,6 +30,17 @@ public class PersonController {
 	@GetMapping("")
 	public List<Person> findAllPersons() {
 		return personService.getAllPersons();
+	}
+	
+	
+	@PostMapping("")
+	public void addPersons(@RequestBody Person person) {
+		try {
+			personService.addPerson(person);
+		}
+		catch (IOException e) {
+			
+		}
 	}
 
 }
